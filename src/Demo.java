b2f1ddf9577acb0d8897a7283a34279b607e3890
Sergey Spiridovich <raboch2014@mail.ru>
2022-04-21 18:21:55 +0300

    add new change

--- src/Demo.java
+++ src/Demo.java
@@ -1,6 +1,6 @@
 public class Demo {
     void fatalErorWithConnekting(){
-        System.out.println("master branch");
+        System.out.println("master branch, changes");
         System.out.println("enother");
     }
 }
