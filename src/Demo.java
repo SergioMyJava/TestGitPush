44e89a535de27e82bf91e5462370f8b7294a1ca6
Sergey Spiridovich <raboch2014@mail.ru>
2022-04-21 18:10:44 +0300

    Differents bitween demo_branch and local_branch

--- src/Demo.java
+++ src/Demo.java
@@ -1,6 +1,7 @@
 public class Demo {
     void fatalErorWithConnekting(){
-        System.out.println("master branch");
+        System.out.println("local_branch branch");
         System.out.println("enother");
+        System.out.println("for merg differents");
     }
 }
