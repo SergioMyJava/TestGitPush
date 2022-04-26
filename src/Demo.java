d06bc05ef76699717c4c6f14d6362bc2403de68b
Sergey Spiridovich <raboch2014@mail.ru>
2022-04-26 17:53:28 +0300

    add new funktion fo test merg master

--- src/Demo.java
+++ src/Demo.java
@@ -3,4 +3,7 @@ public class Demo {
         System.out.println("master branch, changes");
         System.out.println("enother");
     }
+    void newFuktionForMaster(){
+        System.out.println("string for master");
+    }
 }
