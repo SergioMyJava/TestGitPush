2c013762dd69d2d44a1813c8fe6383ffe52827f1
Sergey Spiridovich <raboch2014@mail.ru>
2022-04-26 17:50:53 +0300

    add new funktion fo test merg

--- src/Demo.java
+++ src/Demo.java
@@ -1,7 +1,12 @@
 public class Demo {
-    void fatalErorWithConnekting(){
+    void fatalErorWithConnekting() {
         System.out.println("local_branch branch");
         System.out.println("enother");
         System.out.println("for merg differents");
     }
+
+    void newFunktion() {
+        System.out.println("new fuktion in lokal");
+    }
+
 }
