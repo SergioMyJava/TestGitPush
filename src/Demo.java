01897b295a0f62afdc85ac4cbf190d16467fd731
Sergey Spiridovich <raboch2014@mail.ru>
2022-04-26 18:54:34 +0300

    add new funktion fo test merg master

--- src/Demo.java
+++ src/Demo.java
@@ -1,9 +1,7 @@
 public class Demo {
     void fatalErorWithConnekting(){
+        System.out.println("master branch, changes");
         System.out.println("change_demo branch");
         System.out.println("enother");
-    }
-    void newFunktionForDemobranch(){
-        System.out.println("for demo branch");
     }
 }
